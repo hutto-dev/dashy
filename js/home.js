66d980770ee12474d4e20d47df701a7563dca575
Tori Hutto <183306074+hutto-dev@users.noreply.github.com>
2026-01-29 15:36:09 -0500

    Verse of day working

--- js/home.js
+++ js/home.js
@@ -1,10 +1,31 @@
 ////////////////////////////////////////// VERSE OF THE DAY //////////////////////////////////////////
 
+/////// GRABBING DAILY VERSE ///////
+const verseList = [
+  "PSA.119.45",
+  "PSA.119.32",
+  "PSA.116.7",
+  "PSA.37.3",
+  "PSA.37.4",
+  "PSA.32.8",
+  "PSA.34.4",
+  "PSA.25.12",
+  "PSA.25.14",
+];
+
+const date = new Date();
+const dateNumber =
+  date.getFullYear() * 10000 + (date.getMonth() + 1) * 100 + date.getDate();
+
+const todayNumber = dateNumber % verseList.length;
+const randomVerse = verseList[todayNumber];
+
+console.log(todayNumber);
+
 /////// API ///////
-async function fetchVerses() {
+async function fetchVerses(verseId) {
   const apiKey = "RtBn4DEljTrIU0VBsqsx-";
   const bibleId = "65eec8e0b60e656b-01";
-  const verseId = "GEN.12.8";
   const url = `https://rest.api.bible/v1/bibles/${bibleId}/verses/${verseId}?content-type=text`;
 
   const response = await fetch(url, {
@@ -16,18 +37,25 @@ async function fetchVerses() {
   const data = await response.json();
 
   return {
-    verse: data.data.content.replace(/^\s*\[\d+\]\s*/, "");
-    reference: data.data.reference;
-  }
+    verse: data.data.content.replace(/^\s*\[\d+\]\s*/, ""),
+    reference: data.data.reference,
+  };
 }
-
 
 /////// HTML ///////
-function updateVerseHTML(verse, reference){
-const verseHTML = document.getElementById("verse");
-const refHTML = document.getElementById("ref");
+function updateVerseHTML({ verse, reference }) {
+  const verseHTML = document.getElementById("verse");
+  const refHTML = document.getElementById("ref");
 
-verseHTML.textContent = verse;
-refHTML.textContent = reference;
-
+  verseHTML.textContent = verse;
+  refHTML.textContent = reference;
 }
+
+/////// TYING IT ALL TOGETHER?? ///////
+async function loadVerse() {
+  const verseId = randomVerse;
+  const data = await fetchVerses(verseId);
+  updateVerseHTML(data);
+}
+
+loadVerse();
